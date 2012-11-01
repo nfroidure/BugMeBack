0a92ce57895bc798be9ec645125fab97ec554087
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-11-01 16:48:13 +0100

    Removing old files

--- get_infos.js
+++
@@ -1,7 +0,0 @@
-var bugInfo=document.querySelectorAll('link[rel=bugreport]');
-
-chrome.extension.sendRequest({'report':(bugInfo[0]&&bugInfo[0].hasAttribute('href')?bugInfo[0].getAttribute('href'):''),
-	'reportType':(bugInfo[0]&&bugInfo[0].hasAttribute('type')?bugInfo[0].getAttribute('type'):''),
-	'screen':'width:'+window.innerWidth+', height:'+window.innerHeight+', scrollX:'+window.scrollX+', scrollY:'+window.scrollY
-	,'messages':console.history
-	});
